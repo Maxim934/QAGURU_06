7a4f6aea888ffd213a6d19c168bc648894d96e9c
maxim934 <kimmaxim121@gmail.com>
2022-01-20 15:41:12 +0600

    new test

---
+++ src/test/java/maxim/kim/DownloadTest.java
@@ -0,0 +1,34 @@
+package maxim.kim;
+
+import com.codeborne.selenide.Configuration;
+import org.apache.commons.io.IOUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class DownloadTest {
+    @BeforeAll
+    public static void config() {
+        Configuration.startMaximized = true;
+        Configuration.downloadsFolder = "resources";
+    }
+
+    @Test
+    public void downloadFile() throws IOException, URISyntaxException {
+        open("https://the-internet.herokuapp.com/download");
+       File file = $("a[href=\"download/model.txt\"]").download();
+       String content = IOUtils.toString(new FileReader(file));
+        System.out.println(content);
+       Assertions.assertTrue(content.contains("Age: Under 25, 25-60, Older than 60"));
+    }
+
+}
--- src/test/java/maxim/kim/tests/TestData.java
+++
@@ -1,7 +0,0 @@
-package maxim.kim.tests;
-
-public class TestData {
-    public static String adminLogin = "admin1";
-    public static String adminPassword = "[9k<k8^z!+$$GkuP";
-
-}
